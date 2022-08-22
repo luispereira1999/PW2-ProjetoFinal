03c45c6d2a0de1a1b71e262104a98f2635b1b126
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2022-08-22 09:53:14 +0100

    Rota para a página de login e signup

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 require_once("src/route.php");
+
 require_once("src/controllers/brief-post-controller.php");
+require_once("src/controllers/user-controller.php");
 
 $route = new Route();
 $briefPostController = new BriefPostController();
+$userController = new UserController();
 
 // rotas
-$route->add("/", "src/controllers/brief-post-controller.php", array($briefPostController, "getBriefPosts"));
+$route->add("/", "src/controllers/brief-post-controller.php", array($briefPostController, "index"));
+$route->add("/login-signup", "src/controllers/user-controller.php", array($userController, "loginSignup"));
 
 // quando nenhuma rota foi encontrada
 $route->notFound("src/views/404.php");
