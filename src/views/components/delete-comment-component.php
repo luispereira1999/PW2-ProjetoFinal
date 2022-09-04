dbe4f22745ac658ba090500692cb15a012c383fc
Luís Pereira <67525203+luispereira1999@users.noreply.github.com>
2022-09-04 11:27:16 +0100

    Design da popup de apagar comentário

---
+++ src/views/components/delete-comment-component.php
@@ -0,0 +1,26 @@
+<!-- DEFINIÇÃO: popup de eliminar um comentário -->
+
+<div class="modal fade" id="deleteComment<?= $comments[$current]->comment_id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+   <div class="modal-dialog modal-dialog-centered  modal-lg" role="document">
+      <div class="modal-content">
+         <div class="modal-header">
+            <h5 class="modal-title">Eliminar Comentário</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+            </button>
+         </div>
+
+         <div class="modal-body">
+            <p class="popup__paragraph">Deseja mesmo eliminar este post?</p>
+         </div>
+
+         <div class="modal-footer">
+            <form id="formDeleteComment<?= $comments[$current]->comment_id; ?>" method="post" action="/comment/delete/<?= $comments[$current]->comment_id; ?>">
+               <input type="hidden" name="postId" value="<?= $post->post_id ?>">
+               <button class="button button-primary" type="submit" form="formDeleteComment<?= $comments[$current]->comment_id; ?>" name="isDelete">Eliminar</button>
+               <button class="button button-cancel" type="button">Não</button>
+            </form>
+         </div>
+      </div>
+   </div>
+</div>
